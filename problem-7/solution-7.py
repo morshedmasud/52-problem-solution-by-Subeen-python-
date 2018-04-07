6c4553a8a69b552608852012dc990d4ea12780c6
Masud Morshed <masudraj6@gmail.com>
2018-04-08 00:37:26 +0600

    Solutin 7 added

---
+++ problem-7/solution-7.py
@@ -0,0 +1,9 @@
+# Taking value of 't' for test case
+t = int(input())
+x = 0
+while x < t:
+    # Taking value of 'n' for find length of 'n'
+    n = list(map(int, input().split()))
+    # Using len() function for count length of 'n'
+    print(len(n), "\n")
+    x += 1
