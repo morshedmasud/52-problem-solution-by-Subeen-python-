7e8b3e94b2bb10dfe273f1e2558e998d3f292fc0
Masud Morshed <masudraj6@gmail.com>
2018-04-07 23:40:12 +0600

    Solution 6 added

---
+++ problem-6/solution-6.py
@@ -0,0 +1,13 @@
+# Taking value of 't' from user for test case
+t = int(input())
+x = 0
+while x < t:
+    # Taking value of 'n' from user for sum
+    n = int(input())
+    lsd = n % 10
+    while n > 0:
+        msd = n % 10
+        temp = n // 10
+        n = temp
+    print("Sum =",(lsd+msd),"\n")
+    x += 1
