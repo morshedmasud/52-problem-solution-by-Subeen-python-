d2f88a8dbac10e294e140bfc3260c1de5c5a6576
Masud Morshed <masudraj6@gmail.com>
2018-04-07 23:56:36 +0600

    Solution 6 updated

--- problem-6/solution-6.py
+++ problem-6/solution-6.py
@@ -4,10 +4,12 @@ x = 0
 while x < t:
     # Taking value of 'n' from user for sum
     n = int(input())
+    # lsd for last digit
     lsd = n % 10
     while n > 0:
+        # msd for first digit
         msd = n % 10
         temp = n // 10
         n = temp
     print("Sum =",(lsd+msd),"\n")
-    x += 1
+    x += 1g
