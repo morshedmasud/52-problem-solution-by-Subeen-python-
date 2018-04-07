2b4f48de993b1af7f16c35b4da85d78ebc9cdac0
Masud Morshed <masudraj6@gmail.com>
2018-04-07 18:24:53 +0600

    Solution 3 updated

--- problem-3/solution-3.py
+++ problem-3/solution-3.py
@@ -1,8 +1,13 @@
 # showing all positive number between 1 to 1000
+# Counter for new line
+counter = 0
 i = 1000
 while i > 0:
-    print(i, "\t", end='')
+    counter +=1
+    # when value of counter % 5 == 0 is True then print a new line
+    if counter%5 == 0:
+        print(i, "\t", end='\n')
+    else:
+        print(i, "\t", end='')
     i -= 1
-    # when value of n % 5 == 0 is True then print a new line
-    if i % 5 == 0:
-        print()
+
