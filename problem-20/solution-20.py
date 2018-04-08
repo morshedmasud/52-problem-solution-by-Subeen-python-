7264dc1db92da7d4cd7b3c3eda5c53c7174345d8
Masud Morshed <masudraj6@gmail.com>
2018-04-09 02:53:56 +0600

    solution 20 added

---
+++ problem-20/solution-20.py
@@ -0,0 +1,12 @@
+# Taking value of 't' from user for test case
+t = int(input())
+x = 0
+while x < t:
+    # Taking value of 'string' and also make a list using 'split' function
+    string = input().split()
+    count  = 0
+    # count every word in string and increce count value.
+    for i in string:
+        count += 1
+    print("Count = %d \n" %count)
+    x += 1
