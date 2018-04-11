8d2be5a082cb4d48cff3d5d90e742ca8ea874bdc
Masud Morshed <masudraj6@gmail.com>
2018-04-11 16:35:13 +0600

    Solution 42 updated

--- problem-42/solution-42.py
+++ problem-42/solution-42.py
@@ -2,3 +2,13 @@
 t = int(input())
 x = 0
 while x < t:
+    n = int(input())
+    # create a reversed loop
+    for i in range(n, 0, -1):
+        if i == 1:
+            print("%d\n" %i)
+        elif i == 2:
+            print("%d + " %i, end='')
+        else:
+            print("2^%d + " %i, end='')
+    x += 1
