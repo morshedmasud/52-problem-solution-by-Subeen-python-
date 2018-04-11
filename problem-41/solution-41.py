bfefb3e73d39dbf20249b35654de0cafb2c3c171
Masud Morshed <masudraj6@gmail.com>
2018-04-11 16:20:13 +0600

    Solution 41 updated

--- problem-41/solution-41.py
+++ problem-41/solution-41.py
@@ -2,3 +2,15 @@
 t = int(input())
 x = 0
 while x < t:
+    # Create a function for return 'n' factorial
+    def fact(n):
+        temp = 1
+        for i in range(1, n+1):
+            temp *= i
+        return temp
+    n = int(input())
+    total = 0
+    for i in range(1, n+1):
+        total += i / fact(i)
+    print("%.4f \n" %total)
+    x += 1
